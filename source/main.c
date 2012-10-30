72036969db65e06195b7e8aaea8210648050530c
Paul Berens <pharaun666@gmail.com>
2012-10-30 01:44:00 -0700

    We got a working console, however! we got LOVERLY corruption of the main darwin background, and i've spent about two hours on this thing, i give up

--- source/main.c
+++ source/main.c
@@ -33,24 +33,22 @@ int main(void) {
 	touchPosition touchXY;
 	int i;
 
-	/* Set the mode for 2 text layers and two extended background layers */
-	videoSetMode(MODE_5_2D);
+	/* Force the main engine to top screen */
+	lcdMainOnTop();
 
-	/* Set the mode for text display */
-	videoSetModeSub(MODE_0_2D);
+	/* Set the mode for 2 text layers and two extended background layers */
+	videoSetMode(MODE_5_2D | DISPLAY_BG1_ACTIVE | DISPLAY_BG3_ACTIVE);
+
+	/* Set the mode for sprite display */
+	videoSetModeSub(MODE_0_2D | DISPLAY_SPR_ACTIVE);
 
 	vramSetBankA(VRAM_A_MAIN_BG);
-	vramSetBankC(VRAM_C_SUB_BG);
+	vramSetBankB(VRAM_B_MAIN_BG);
 	vramSetBankD(VRAM_D_SUB_SPRITE);
 
 	/* Set the default backgorund color */
 	setBackdropColor(0xF);
 	setBackdropColor(0xF);
-
-	/* Set up the console */
-	PrintConsole topScreen;
-	consoleInit(&topScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
-	consoleSelect(&topScreen);
 
 	/* Iniitate the sprite engine */
 	oamInit(&oamSub, SpriteMapping_1D_32, false);
@@ -59,6 +57,11 @@ int main(void) {
 	/* Decompress and show the logo */
 	int bg3 = bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
 	decompress(logoBitmap, BG_GFX, LZ77Vram);
+
+	/* Set up the console */
+	PrintConsole topScreen;
+	int bg1 = bgInit(1, BgType_Text4bpp, BgSize_T_256x256, 0, 0);
+	consoleInit(&topScreen, 1, BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
 
 	irqSet(IRQ_VBLANK, Vblank);
 
@@ -78,10 +81,10 @@ int main(void) {
 	boxOfDoom.gfx_buffer = gfx;
 
 	Position targetPos;
-	targetPos.x = 32; 
+	targetPos.x = 32;
 	targetPos.y = 32;
 
-	boxOfDoom.pos.x = 16; 
+	boxOfDoom.pos.x = 16;
 	boxOfDoom.pos.y = 16;
 
 	/* Hide title */
@@ -93,7 +96,9 @@ int main(void) {
 		if (keysUp() & KEY_START) bg3_hidden = !bg3_hidden;
 		if (bg3_hidden) {
 			bgHide(bg3);
+			bgShow(bg1);
 		} else {
+			bgHide(bg1);
 			bgShow(bg3);
 		}
 
