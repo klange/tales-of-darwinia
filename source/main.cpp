b70f0909b3532d8d7bc40b3f06b203d166a5449b
Paul Berens <pharaun666@gmail.com>
2012-10-30 23:12:35 -0700

    Initial sample map commit

--- source/main.cpp
+++ source/main.cpp
@@ -16,85 +16,99 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include "logo.h"
-#include "vector3.h"
 
-volatile int frame = 0;
+//create a tile called redTile
+u8 redTile[64] =
+{
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1
+};
 
-void Vblank() {
-	frame++;
-	scanKeys();
-}
+//create a tile called greenTile
+u8 greenTile[64] =
+{
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2
+};
+
+// Map
+u16 map[1024] =
+{
+	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,
+	1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,
+	1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,
+	1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,
+	1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,
+	1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0
+};
+
 
 int main(void) {
-	/* Touchscreen position */
-	touchPosition touchXY;
+	//set video mode and map vram to the background
+	videoSetMode(MODE_0_2D | DISPLAY_BG0_ACTIVE);
+	vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
 
-	/* Force the main engine to top screen */
-	lcdMainOnTop();
+	//get the address of the tile and map blocks
+	u8* tileMemory = (u8*)BG_TILE_RAM(1);
+	u16* mapMemory = (u16*)BG_MAP_RAM(0);
 
-	/* Set the mode for 2 text layers and two extended background layers */
-	videoSetMode(MODE_5_2D | DISPLAY_BG1_ACTIVE | DISPLAY_BG3_ACTIVE);
-	vramSetBankA(VRAM_A_MAIN_BG);
-	vramSetBankB(VRAM_B_MAIN_BG);
+	//tell the DS where we are putting everything and set 256 color mode and that we are using a 32 by 32 tile map.
+	REG_BG0CNT = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(0) | BG_TILE_BASE(1);
 
-	/* Set the mode for sprite display */
-	videoSetModeSub(MODE_5_2D | DISPLAY_SPR_ACTIVE);
-	vramSetBankC(VRAM_C_SUB_BG);
-	vramSetBankD(VRAM_D_SUB_SPRITE);
+	//load our palette
+	BG_PALETTE[1] = RGB15(31,0,0);
+	BG_PALETTE[2] = RGB15(0,31,0);
 
-	/* Set the default backgorund color */
-	setBackdropColor(0xF);
 
-	/* Decompress and show the logo */
-	int bg3 = bgInitSub(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
-	decompress(logoBitmap, BG_GFX_SUB, LZ77Vram);
+	//copy the tiles into tile memory one after the other
+	swiCopy(redTile, tileMemory, 32);
+	swiCopy(greenTile, tileMemory + 64, 32);
 
-	/* Set up the console */
-	PrintConsole topScreen;
-	int bg1 = bgInit(1, BgType_Text4bpp, BgSize_T_256x256, 0, 0);
-	consoleInit(&topScreen, 1, BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
+	// copy the map into map memory
+	swiCopy(map, mapMemory, 1024); // Half word = 16bit
 
-	/* Set the vertical blank event */
-	irqSet(IRQ_VBLANK, Vblank);
-
-	/* Hide title */
-	bool bg3_hidden = false;
-
-	Vector3<u16>* derp = new Vector3<u16>(1, 2, 3);
-
-	while(1) {
+	int scrollY = 64;
+	while(1)
+		REG_BG0VOFS = scrollY;
 		swiWaitForVBlank();
-
-		SPRITE_PALETTE_SUB[1] = RGB15(0, frame % 32, 0);
-
-		if (keysUp() & KEY_START) bg3_hidden = !bg3_hidden;
-		if (bg3_hidden) {
-			bgHide(bg3);
-			bgShow(bg1);
-		} else {
-			bgHide(bg1);
-			bgShow(bg3);
-		}
-
-		// This section below is for touch screen sprite
-		if (keysHeld() & KEY_TOUCH) {
-			touchRead(&touchXY);
-		}
-
-		// WHY WON'T YOU WORK WTF :(
-		derp->setX(touchXY.px);
-		derp->setY(touchXY.py);
-
-		// Y U NO WORK
-		u16 asdf[2];
-		asdf[0] = touchXY.px;
-		asdf[1] = touchXY.py;
-
-		iprintf("\033[23;0H%d  %d,%d", frame, asdf[0], asdf[1]);
-
-		bgUpdate();
-	}
 
 	return 0;
 }
