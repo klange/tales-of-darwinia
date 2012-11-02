300c31eacee5538610a2f42ccbc74cc9acbbd635
Pat <tiet.patrick@gmail.com>
2012-11-02 01:55:56 -0700

    Fix health bug

--- include/livingentity.h
+++ include/livingentity.h
@@ -8,10 +8,8 @@
 class LivingEntity : public Sprite
 {
 public:
-	LivingEntity(SpriteData *gfx) : Sprite(gfx) {
-		mStats->health = 4; 
-		mStats->attack = 1;
-	}
+	LivingEntity(SpriteData *gfx) : Sprite(gfx) {}
+	virtual void Init();
 	virtual void Damage(u16 damage);
 	virtual void OnDeath() {};
 
